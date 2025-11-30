38bfec00fde9e6952d78546229131a27782d08c1
wassfila <wassim.filali@gmail.com>
2025-11-30 15:00:35 +0100

    added light caching with blob_id in Etag

--- src/pages/assets/[...uid].js
+++ src/pages/assets/[...uid].js
@@ -2,7 +2,7 @@ import {config} from '@/config';
 import { file_mime } from '@/libs/utils.js';
 import {getAssetWithBlob} from '@/libs/structure-db.js';
 
-export async function GET({params}) {
+export async function GET({params, request}) {
     if (config.copy_assets) {
         return new Response('Not supported and not needed with copy_assets = true', {status: 404});
     }
@@ -14,9 +14,23 @@ export async function GET({params}) {
     if (asset && buffer) {
         const filename = asset.path ? asset.path.split(/[\\/]/).pop() : null;
         const contentType = file_mime(asset.ext ?? asset.path ?? uid);
+        const etag = asset.blob_uid ;
+        if (request?.headers?.get('if-none-match') === etag) {
+            return new Response(null, {
+                status: 304,
+                headers: {
+                    'Cache-Control': 'private, max-age=0, no-cache',
+                    'ETag': etag,
+                    'Vary': 'Authorization, Cookie'
+                }
+            });
+        }
         const headers = {
             'Content-Type': contentType,
-            'Content-Length': buffer.length?.toString()
+            'Content-Length': buffer.length?.toString(),
+            'Cache-Control': 'private, max-age=0, no-cache',
+            'ETag': etag,
+            'Vary': 'Authorization, Cookie',
         };
         if (filename) {
             headers['Content-Disposition'] = `inline; filename="${filename}"`;
